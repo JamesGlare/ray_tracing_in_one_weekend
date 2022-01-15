5acfae4d14c84c0e257f4feffd525287925625bc
JamesGlare <jannesgla@gmail.com>
2022-01-15 14:54:58 +0100

    add set_face_normal

--- inc/sphere.hpp
+++ inc/sphere.hpp
@@ -1,7 +1,7 @@
 #ifndef SPHERE_H
 #define SPHERE_H
 #include "hittable.hpp"
-#include "vec3.h"
+#include "vec3.hpp"
 
 class sphere : public hittable
 {
@@ -20,7 +20,7 @@ public:
 bool sphere::hit(const ray &r, double t_min, double t_max, hit_record &rec) const
 {
     vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared;
+    auto a = r.direction().length_squared();
     auto half_b = dot(oc, r.direction());
     auto c = oc.length_squared() - radius * radius;
     auto discriminant = half_b * half_b - a * c;
@@ -36,7 +36,8 @@ bool sphere::hit(const ray &r, double t_min, double t_max, hit_record &rec) cons
     }
     rec.t = root;
     rec.p = r.at(rec.t);
-    rec.normal = (rec.p - center) / radius;
+    vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
     return true;
 }
 
